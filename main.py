4f2d5810d7300bc8199b9b700f0475a5a5bca496
Gilberto Santos <gilberto.santos.96@gmail.com>
2023-01-31 12:50:44 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print('Opções de Produto\n1- hambúrguer\n2- cachorro quente\n3- salgadinho\n4- prato feito')
+escolha_lanche = int(input('Digite o código do produto '))
+valor_lanche = 0
+print('Opções de Produto\n1- água\n2- refrigerante\n3-refresco\n4-suco')
+escolha_bebida = int(input('Digite o código do produto '))
+valor_bebida = 0
