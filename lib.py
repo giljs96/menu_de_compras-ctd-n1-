18b6d4fd4066a4a8ce929f4c3015e2d37e607f55
Gilberto Santos <gilberto.santos.96@gmail.com>
2023-02-18 18:07:52 -0300

    commit n3

---
+++ lib.py
@@ -0,0 +1,13 @@
+class Produto:
+
+    def __init__(self, id, nome, valor):
+        self.id = id
+        self.nome = nome
+        self.valor = valor
+
+class FormaPagamento:
+
+    def __init__(self, id, nome):
+        self.id = id
+        self.nome = nome
+        
